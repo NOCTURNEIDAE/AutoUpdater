fbfa9fc2281625f1b95e4020885cfea3f01ead76
Nathan Roy <nathanroy1@gmail.com>
2023-02-01 11:49:23 -0500

    Created network.py

---
+++ src/network.py
@@ -0,0 +1,19 @@
+# NETWORK
+installers = "\\\\SPS-MTL-JUPITER\\sps-mtl-bank\\SPS Bank Installers"
+
+exe_dict = {
+
+	'CrossFTP': 		installers+"\\CrossFTP\\Windows\\crossftp-full-setup-1.99.9.exe",
+	'CyberDuck': 		installers+"\\Cyberduck\\Windows\\Cyberduck-Installer-8.4.3.38269.exe",
+	'FreeFileSync': 	installers+"\\FreeFileSync\\Windows\\FreeFileSync_11.29_Windows_Setup.exe",
+	'HandBrake': 		installers+"\\HandBrake\\Windows\\HandBrake-1.5.1-x86_64-Win_GUI.exe",
+	'MountainDuck': 	installers+"\\MountainDuck\\Windows\\Mountain Duck Installer-4.12.2.20039.exe",
+	'MaxonApp': 		installers+"\\Maxon\\Maxon App\\Maxon_App_2023.1.2_Win",
+	'Notch': 			installers+"\\Notch\\Windows\\notch_x64_0923_rc1802.exe",
+	'Nvidia': 			installers+"\\Nvidia\\Windows\\527.56-desktop-win10-win11-64bit-international-nsd-dch-whql.exe",
+	'Parsec': 			installers+"\\Parsec\\Windows\\parsec_windows_150-86h.exe",
+	'Pulseway': 		installers+"\\Pulseway\\Windows\\Pulseway_9.0.1_x64.msi",
+	'TeamViewer': 		installers+"\\TeamViewer\\Windows\\TeamViewer_Setup_x64.exe",
+	'VLC': 				installers+"\\VLC\\Windows\\vlc-3.0.18-win64.exe",
+}
+
